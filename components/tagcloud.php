38c8071abd22330d46b29d2dcf8b2b0c05a21238
laozhu <devzsj@163.com>
2024-05-27 16:16:06 +0800

    优化样式，防止标签云越界

--- components/tagcloud.php
+++ components/tagcloud.php
@@ -1,9 +1,7 @@
 <?php if (!empty($this->options->sidebarBlock) && in_array('ShowTagCloud', $this->options->sidebarBlock)) : ?>
     <section class="bg-white rounded shadow-sm mt-3">
         <h3 class="px-3 py-2 m-0 fs-6 text-bg-light rounded-top"> <i class="iconfont icon-tags-fill me-2"></i><?php _e('标签云'); ?></h3>
-        <div>
-            <canvas id="tag-cloud" height="300"></canvas>
-        </div>
+        <canvas id="tag-cloud" height="300" style="width: 100%;"></canvas>
         <div id="side-tags" class="pjax" style="display: none;">
             <?php \Widget\Metas\Tag\Cloud::alloc()
                 ->parse('<a href="{permalink}">{name}</a>'); ?>
